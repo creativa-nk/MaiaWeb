49fabce2e1045e22c5821c58ef6027cb951861bd
daniel salgado <daniel.salgado.p@gmail.com>
2021-05-14 00:21:33 +0200

    mvp

---
+++ js/portFolioLoader.js
@@ -0,0 +1,125 @@
+var portFolio = `
+ <div class="item">
+ <img src="./img/condeDeGitanes.jpg" alt="NAME" class="IMGresponsive" data-aos="fade-right">
+ <a href="./img/condeDeGitanes.jpg" class="zoom valign-center">
+     <div class="valign-center-elem">
+         <strong>Madrid City Project</strong>
+         <!-- <em>Property</em> -->
+         <b>Details</b>
+     </div>
+ </a>
+</div>
+<div class="item">
+ <img src="./img/moraleja.jpg" alt="NAME" class="IMGresponsive" data-aos="fade-left">
+     <a href="./img/moraleja.jpg" class="zoom valign-center">
+     <div class="valign-center-elem">
+         <strong>Madrid City Project</strong>
+         <!-- <em>Property</em> -->
+         <b>Details</b>
+     </div>
+ </a>
+</div>
+<div class="item">
+ <img src="./img/SALON.jpg" alt="NAME" class="IMGresponsive" data-aos="fade-right">
+     <a href="./img/SALON.jpg" class="zoom valign-center">
+     <div class="valign-center-elem">
+         <strong>Madrid City Project</strong>
+         <!-- <em>Property</em> -->
+         <b>Details</b>
+     </div>
+ </a>
+</div>
+<div class="item">
+ <img src="./img/valdemartin.jpg" alt="NAME" class="IMGresponsive" data-aos="fade-left">
+ <a href="./img/valdemartin.jpg" class="zoom valign-center">
+     <div class="valign-center-elem">
+         <strong>Madrid City Project</strong>
+         <!-- <em>Property</em> -->
+         <b>Details</b>
+     </div>
+ </a>
+</div>
+<div class="item">
+ <img src="./img/AceroCorten.jpg" alt="NAME" class="IMGresponsive" data-aos="fade-right">
+ <a href="./img/AceroCorten.jpg" class="zoom valign-center">
+     <div class="valign-center-elem">
+         <strong>Madrid City Project</strong>
+         <!-- <em>Property</em> -->
+         <b>Details</b>
+     </div>
+ </a>
+</div>
+<div class="item">
+ <img src="./img/salon2.jpg" alt="NAME" class="IMGresponsive" data-aos="fade-left">
+ <a href="./img/salon2.jpg" class="zoom valign-center">
+     <div class="valign-center-elem">
+         <strong>Madrid City Project</strong>
+         <!-- <em>Property</em> -->
+         <b>Details</b>
+     </div>
+ </a>
+</div>
+<div class="item">
+ <img src="./img/condeDeGitanes.jpg" alt="NAME" class="IMGresponsive" data-aos="fade-right">
+ <a href="./img/condeDeGitanes.jpg" class="zoom valign-center">
+     <div class="valign-center-elem">
+         <strong>Madrid City Project</strong>
+         <!-- <em>Property</em> -->
+         <b>Details</b>
+     </div>
+ </a>
+</div>
+<div class="item">
+ <img src="./img/AceroCorten.jpg" alt="NAME" class="IMGresponsive" data-aos="fade-left">
+ <a href="./img/AceroCorten.jpg" class="zoom valign-center">
+     <div class="valign-center-elem">
+         <strong>Madrid City Project</strong>
+         <!-- <em>Property</em> -->
+         <b>Details</b>
+     </div>
+ </a>
+</div>
+<div class="item">
+ <img src="./img/valdemartin.jpg" alt="NAME" class="IMGresponsive" data-aos="fade-right">
+ <a href="./img/valdemartin.jpg" class="zoom valign-center">
+     <div class="valign-center-elem">
+         <strong>Madrid City Project</strong>
+         <!-- <em>Property</em> -->
+         <b>Details</b>
+     </div>
+ </a>
+</div>
+<div class="item">
+ <img src="./img/salon2.jpg" alt="NAME" class="IMGresponsive" data-aos="fade-left">
+ <a href="./img/salon2.jpg" class="zoom valign-center">
+     <div class="valign-center-elem">
+         <strong>Madrid City Project</strong>
+         <!-- <em>Property</em> -->
+         <b>Details</b>
+     </div>
+ </a>
+</div>
+<div class="item">
+ <img src="./img/moraleja.jpg" alt="NAME" class="IMGresponsive" data-aos="fade-right">
+ <a href="./img/moraleja.jpg" class="zoom valign-center">
+     <div class="valign-center-elem">
+         <strong>Madrid City Project</strong>
+         <!-- <em>Property</em> -->
+         <b>Details</b>
+     </div>
+ </a>
+</div>
+<div class="item">
+ <img src="./img/SALON.jpg" alt="NAME" class="IMGresponsive" data-aos="fade-left">
+ <a href="./img/SALON.jpg" class="zoom valign-center">
+     <div class="valign-center-elem">
+         <strong>Madrid City Project</strong>
+         <!-- <em>Property</em> -->
+        <b>Details</b>
+     </div>
+ </a>
+</div>`;
+// document.getElementById('portfolio-contailer').innerHTML=portFolio;
+var element = document.getElementById('portfolio-container');
+console.log(element);
+element.innerHTML=portFolio;
