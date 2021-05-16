32c02b381e795d3a294f8687c23a6a2039a799af
daniel salgado <daniel.salgado.p@gmail.com>
2021-05-16 17:52:32 +0200

    working! hace falta meter los datos en const items

--- js/portfolioItems.js
+++ js/portfolioItems.js
@@ -1,15 +1,7 @@
-{/* <img src="./img/condeDeGitanes.jpg" alt="NAME" class="IMGresponsive" data-aos='fade-right'>
-<a href="./img/condeDeGitanes.jpg" class="zoom valign-center">
-    <div class="valign-center-elem">
-        <strong>Madrid City Project</strong>
-        <!-- <em>Property</em> -->
-        <b>Details</b>
-    </div>
-</a> */}
 const IMG_PATH = './img';
 const IMG_CLASS = 'IMGresponsive';
 const LINK_CLASS = ['zoom', 'valign-center']
-const DIV_CLASS = 'valign-center-elem'
+const DIV_CLASS = ['valign-center-elem']
 const items = [
     {
         src: 'condeDeGitanes.jpg',
@@ -31,28 +23,57 @@ const items = [
 function buildItems(items) {
     let fragment = new DocumentFragment();
     for (key in items) {
-        // console.log(items[key]);
-        let img = builImgFromItem(items[key]);
+        let divItem = document.createElement('div');
+        divItem.classList.add('item');
+        fragment.appendChild(divItem);
+        let img = builImgFromItem(items[key], key);
         let a = buildLinkFromItem(items[key]);
-        fragment.appendChild(a);
-        fragment.insertBefore(img,a);
+        let div = buildDivFromItem(items[key]);
+        divItem.appendChild(img)
+        divItem.appendChild(a);
+        a.appendChild(div)
     }
     return fragment;
 }
 
 function buildLinkFromItem(item){
     let a =  document.createElement('a');
-    a.href=item.src;
+    a.href = IMG_PATH + '/' + item.src;
     a.classList.add(...LINK_CLASS);
     return a
 }
-
-function builImgFromItem(item) {
+{/* <img src="./img/condeDeGitanes.jpg" alt="NAME" class="IMGresponsive" data-aos='fade-right'>
+<a href="./img/condeDeGitanes.jpg" class="zoom valign-center">
+    <div class="valign-center-elem">
+        <strong>Madrid City Project</strong>
+        <!-- <em>Property</em> -->
+        <b>Details</b>
+    </div>
+</a> */}
+function builImgFromItem(item, key) {
     let img = document.createElement('img');
     img.src = IMG_PATH + '/' + item.src;
     img.alt = item.alt;
+    img.classList.add('IMGresponsive');
+    if(key%2 == 0){
+        img.dataset.aos = 'fade-left';
+    } else {
+        img.dataset.aos = 'fade-right';
+    }
     // console.log(img);
     return img;
+}
+
+function buildDivFromItem(item) {
+    let div = document.createElement('div');
+    div.classList.add(...DIV_CLASS);
+    let strong = document.createElement('strong');
+    strong.innerHTML = item.message;
+    let b = document.createElement('b');
+    b.innerHTML = 'Details'
+    div.appendChild(strong);
+    div.appendChild(b);
+    return div
 }
 
 
