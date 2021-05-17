85567aa5857d65ce0d331d3f8fdb5bc42f4080f7
Nadia <nadiyaklokova@gmail.com>
2021-05-18 00:20:15 +0200

    menu bar mobil

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("nav").style.backgroundColor = "white";
+    document.getElementsByClassName('btn-menu').style.color ='black';
   } else {
     document.getElementById("nav").style.backgroundColor = "transparent";
+    document.getElementsByClassName("menu_bar").style.backgroundColor = "transparent";
   }
 }
