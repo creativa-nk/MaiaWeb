48dafab84e194ea090ab2bd323ac9ba5776fa76c
Nadia <nadiyaklokova@gmail.com>
2021-05-23 23:28:09 +0200

    menu desaparece con scroll

--- js/main.js
+++ js/main.js
@@ -16,3 +16,16 @@ function toggleMenu(){
 }
 
 toggle.addEventListener('click', toggleMenu, false);
+
+/* menu scroll down */
+let ubicacionPrincipal  = window.pageYOffset;
+window.onscroll = function() {
+    let Desplazamiento_Actual = window.pageYOffset;
+    if(ubicacionPrincipal >= Desplazamiento_Actual){
+        document.getElementById('navbar').style.top = '0';
+    }
+    else{
+        document.getElementById('navbar').style.top = '-5em';
+    }
+    ubicacionPrincipal = Desplazamiento_Actual;
+}
