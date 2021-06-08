b6e035e8db2c7d627198a09a7c5ae22c513add72
Nadia <nadiyaklokova@gmail.com>
2021-06-08 10:42:44 +0200

    arreglado ...leer mas

--- js/main.js
+++ js/main.js
@@ -60,3 +60,20 @@ function vermas(id){
     document.getElementById("mas").style.display="inline";
     }
 }
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      document.getElementById("mas").style.display="inline";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      document.getElementById("mas").style.display="none";
+      document.getElementById("menos").style.display="inline";
+      moreText.style.display = "inline";
+    }
+  }
