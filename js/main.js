1d6462cd62685301a7556d1e41d59139b19685fd
Nadia <nadiyaklokova@gmail.com>
2021-08-19 23:20:44 +0200

    lightbox gallery

--- js/main.js
+++ js/main.js
@@ -68,3 +68,31 @@ function vermas() {
       moreText.style.display = "inline";
     }
   }
+
+
+  /* ====================================LIGHTBOX========================================== */
+
+const images = document.querySelectorAll('.img'); /* todas la imagenes que tengan classe img */
+const containerImage = document.querySelector('.container-img');
+const imageContainer = document.querySelector('.img-show');
+const closeContainer = document.querySelector('.x');
+
+images.forEach(image =>{
+    image.addEventListener('click',() => {
+       addImage(image.getAttribute('src'),/*  image.getAttribute('alt') */);
+      
+      })
+})
+
+/* ------------abrir container------------- */
+const addImage = (srcImage, altImage)=>{
+  containerImage.classList.toggle('move');
+  imageContainer.classList.toggle('show');   /* toggle agrega o quita la clase .move */
+  imageContainer.src = srcImage;
+}
+
+/* ------------cerrar container-------- */
+  closeContainer.addEventListener('click',() =>{ /* cuando cerramos el container */
+  containerImage.classList.toggle('move'); 
+  imageContainer.classList.toggle('show');
+})
