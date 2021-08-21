75845d93654e60fee5be4001dc6ff4934a123949
Nadia <nadiyaklokova@gmail.com>
2021-08-21 12:14:44 +0200

    texto img

--- js/main.js
+++ js/main.js
@@ -78,10 +78,11 @@ const images = document.querySelectorAll('.img'); /* todas la imagenes que tenga
 const containerImage = document.querySelector('.container-img');
 const imageContainer = document.querySelector('.img-show');
 const closeContainer = document.querySelector('.x');
+const copy = document.querySelector('.copy');
 
 images.forEach(image =>{
     image.addEventListener('click',() => {
-       addImage(image.getAttribute('src'),/*  image.getAttribute('alt') */);
+       addImage(image.getAttribute('src'),  image.getAttribute('alt'));
       
       })
 })
@@ -91,6 +92,7 @@ const addImage = (srcImage, altImage)=>{
   containerImage.classList.toggle('move');
   imageContainer.classList.toggle('show');   /* toggle agrega o quita la clase .move */
   imageContainer.src = srcImage;
+  copy.innerHTML = altImage;
 }
 
 /* ------------cerrar container-------- */
